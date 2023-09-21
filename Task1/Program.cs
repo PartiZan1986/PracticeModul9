1ec458a69a433de0d41812e6b67a924ed12d3f8e
PartiZan1986 <56844192+PartiZan1986@users.noreply.github.com>
2023-09-21 21:07:10 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new MyException();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                throw new MyException2();
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                throw new MyException3();
+            }
+
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                throw new MyException4();
+            }
+
+            catch (DriveNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        public class MyException : Exception
+        {
+
+        }
+
+        public class MyException2 : DivideByZeroException
+        {
+
+        }
+
+        public class MyException3 : FileNotFoundException
+        {
+
+        }
+
+        public class MyException4 : DriveNotFoundException
+        {
+
+        }
+
+        public class MyException5 : TimeoutException
+        {
+
+        }
+    }
+    
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberReader reader = new NumberReader();
+            reader.NumberReaderEvent += ShowResult;
+
+            Console.WriteLine("Введите 5 фамилий");
+
+            string[] ArraySurnames = new string[5];
+
+            for (int i = 0; i < ArraySurnames.Length; i++)
+            {
+                ArraySurnames[i] = Console.ReadLine();
+            }
+
+            try
+            {
+                reader.Read();
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено не корректное значение");
+            }
+
+            //Array.Sort(ArraySurnames);
+            //Array.Reverse(ArraySurnames);
+            //Console.WriteLine(String.Join(", ", ArraySurnames));
+
+
+            int ShowResult(int number)
+            {
+                switch (number)
+                {
+                    case 1: Console.WriteLine("Ввели 1"); break;
+                    case 2: Console.WriteLine("Ввели 2"); break;
+                    default: Console.WriteLine(number); break;
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        public class NumberReader
+        {
+            public delegate int NumberReaderDelegate(int value);
+            public event NumberReaderDelegate NumberReaderEvent;
+
+            public void Read()
+            {
+                Console.WriteLine("Введите 1, если хоти отсортировать от А -  Я.\n" + "Введите 2, если хоти отсортировать от Я - А.");
+
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number != 1 && number != 2) throw new FormatException();
+
+                NumberEntered(number);
+            }
+
+            protected virtual void NumberEntered(int number)
+            {
+                NumberReaderEvent.Invoke(number);
+            }
+        }
+    }
+
+}
